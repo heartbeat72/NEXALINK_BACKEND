60613122446eee905ed9f467fdeea04e1c03a42c
heartbeat72 <97636642+heartbeat72@users.noreply.github.com>
2025-04-09 16:57:28 +0530

    urls.py

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,9 +1,4 @@
 from django.urls import path, include
-from rest_framework.routers import Default
-
-
-```python file="analytics/urls.py"
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import EngagementRecordViewSet, PerformanceRecordViewSet, AnalyticsReportViewSet
 
