4bec21e0e60c32286bb4084d2a495c31c02c7083
heartbeat72 <97636642+heartbeat72@users.noreply.github.com>
2025-04-09 16:29:11 +0530

    urls.py

--- users/urls.py
+++ users/urls.py
@@ -4,12 +4,11 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import UserViewSet, CustomTokenObtainPairView, UserPreferenceViewSet
 
 router = DefaultRouter()
-router.register(r'', UserViewSet, basename='user')
+router.register(r'users', UserViewSet, basename='user')  # optional: use 'users' instead of ''
 router.register(r'preferences', UserPreferenceViewSet, basename='user-preference')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_  name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # fixed
 ]
